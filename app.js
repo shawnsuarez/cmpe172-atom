cf4a32d3d762c51bce0698fb1a54342b3ba0ebee
shawnsuarez <shawn.darrel.suarez@gmail.com>
2019-03-03 17:48:23 -0800

    Heroku

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.listen(8080, function() {
-  console.log("NODE server listening on port 8080!");
+app.get("/" , (req, res) => {
+	res.send("Front Page");
+});
+
+app.listen(PORT, () => {
+  console.log(`NODE server listening on port ${ PORT }!`);
 });
