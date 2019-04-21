a33e1a42cc46a033fe61631808ae5dbfc5136d5e
shawnsuarez <shawn.darrel.suarez@gmail.com>
2019-04-21 15:07:17 -0700

    Merged app.js fix from heroku branch

--- client/src/auth0-variables.js
+++ client/src/auth0-variables.js
@@ -2,7 +2,7 @@ export const AUTH_CONFIG = {
   domain: "dev-rlppi4x4.auth0.com",
   roleUrl: "https://rbac-tutorial-app/role",
   clientId: "ML3BDi478nGqejP9FDH5evCLQW8qpSB5",
-  callbackUrl: "https://atom-payroll.herokuapp.com/callback"
+  callbackUrl: "http://localhost:3000/callback"
 };
 
 //https://atom-payroll.herokuapp.com/callback
