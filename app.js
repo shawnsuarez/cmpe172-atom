8932e4b3a7aabbf8be2626f3394da25a1ed915a7
shawnsuarez <shawn.darrel.suarez@gmail.com>
2019-04-21 15:05:35 -0700

    App.js routes fix

--- app.js
+++ app.js
@@ -15,13 +15,22 @@ app.use(function(req, res, next) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const employeeRoutes = require("./routes/employeeRoutes");
+const payrollRoutes = require("./routes/payrollRoutes");
+const departmentRoutes = require("./routes/departmentRoutes");
+
+app.use("/employeestest", employeeRoutes);
+app.use("/dashboard", payrollRoutes);
+//app.use("/payrolltest", payrollRoutes);
+app.use("/departments", departmentRoutes);
+
 // Static file declaration
 app.use("/static", express.static(path.join(__dirname, 'client/build')));
 
 // production mode
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendfile(path.join(__dirname = 'client/build/index.html'));
   })
 };
@@ -39,18 +48,9 @@ if(process.env.NODE_ENV === 'production') {
 
 const root = require('path').join(__dirname, 'client', 'public')
 app.use(express.static(root));
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile('index.html', { root });
 });
-
-const employeeRoutes = require("./routes/employeeRoutes");
-const payrollRoutes = require("./routes/payrollRoutes");
-const departmentRoutes = require("./routes/departmentRoutes");
-
-app.use("/employeestest", employeeRoutes);
-app.use("/dashboard", payrollRoutes);
-app.use("/payrolltest", payrollRoutes);
-app.use("/departments", departmentRoutes);
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
