40bea1b0fe370a650cc7914c1dc7d766417c8058
shawnsuarez <shawn.darrel.suarez@gmail.com>
2019-04-25 16:22:29 -0700

    Merged backend2 for pagination fix

--- client/src/auth0-variables.js
+++ client/src/auth0-variables.js
@@ -2,7 +2,7 @@ export const AUTH_CONFIG = {
   domain: "dev-rlppi4x4.auth0.com",
   roleUrl: "https://rbac-tutorial-app/role",
   clientId: "ML3BDi478nGqejP9FDH5evCLQW8qpSB5",
-  callbackUrl: "http://localhost:3000/callback"
+  callbackUrl: "https://atom-payroll.herokuapp.com/callback"
 };
 
 //https://atom-payroll.herokuapp.com/callback
